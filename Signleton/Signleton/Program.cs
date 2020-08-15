a7c427291129736f3706755f2f7cb115dc988d99
dnomesh <69308542+dnomesh@users.noreply.github.com>
2020-08-16 02:37:46 +0530

    Add files via upload

---
+++ Signleton/Signleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Signleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(new ParallelOptions() { MaxDegreeOfParallelism = 2 },
+                () => StudentDetails(),
+                () => EmployeeDetails()
+                );
+        }
+
+        private static void EmployeeDetails()
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("this is the second message");
+        }
+
+        private static void StudentDetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("this is the first message");
+        }
+    }
+}
---
+++ Signleton/Signleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Signleton
+{
+    public sealed class Singleton
+    {
+        private static readonly object obj = new object();
+        private Singleton()
+        {
+            Console.WriteLine("Singleton 1");
+        }
+
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(()=> new Singleton());
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
